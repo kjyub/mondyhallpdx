0a210443cc1ab467eb033d8401933fdc5164e64f
JunYub Kim <kjyub08@naver.com>
2022-04-17 15:38:56 +0900

    도움말 추가
    링크로 연결

--- src/components/game/GameLogo.tsx
+++ src/components/game/GameLogo.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+const HELP_MONTYHALL_URL = "https://namu.wiki/w/%EB%AA%AC%ED%8B%B0%20%ED%99%80%20%EB%AC%B8%EC%A0%9C"
+
 function GameLogo() {
+    const openHelp = () => {
+        window.open(HELP_MONTYHALL_URL, "_blank")
+    }
+
     return (
         <div className="flex flex-col p-16">
             <div className="flex flex-col">
                 <p className="font-bold text-4xl">MondyHall Pdx</p>
                 <div className='w-full flex justify-end'>
-                    <span className="mt-2 p-1 text-sm hover:bg-yellow-200 hover:ring-1 ring-yellow-500 rounded-lg duration-300">몬티홀의 역설이란 ?</span>
+                    <span className="mt-2 p-1 text-sm hover:bg-yellow-200 hover:ring-1 ring-yellow-500 rounded-lg duration-300" onClick={()=>{openHelp()}}>몬티홀의 역설이란 ?</span>
                 </div>
             </div>
         </div>
