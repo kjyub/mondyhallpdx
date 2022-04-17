0228afd33323c8cf6f6061f0e0e13279564469fd
JunYub Kim <kjyub08@naver.com>
2022-04-17 23:26:38 +0900

    배포

---
+++ build/precache-manifest.812f91064bcd291f22fdfa1d16903fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea1a3a4df52d9202678a",
+    "url": "/mondyhallpdx/static/css/main.ccc1a42d.chunk.css"
+  },
+  {
+    "revision": "ea1a3a4df52d9202678a",
+    "url": "/mondyhallpdx/static/js/main.ea1a3a4d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/mondyhallpdx/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1715448615d97df2a2ae",
+    "url": "/mondyhallpdx/static/js/2.17154486.chunk.js"
+  },
+  {
+    "revision": "63d5655e7256744469c8",
+    "url": "/mondyhallpdx/static/js/runtime~main.63d5655e.js"
+  },
+  {
+    "revision": "dd828b5ca71a81a8ee738f5f1f93618d",
+    "url": "/mondyhallpdx/index.html"
+  }
+];
