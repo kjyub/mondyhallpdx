dad8916482e81d6ee72a35f019a656b17e766616
JunYub Kim <kjyub08@naver.com>
2022-04-17 15:03:26 +0900

    Updates

---
+++ precache-manifest.04f8b3e4c243cb397df32de36d906e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27adc99eb30bf6c6beed",
+    "url": "/mondyhallpdx/static/css/main.da8380d6.chunk.css"
+  },
+  {
+    "revision": "27adc99eb30bf6c6beed",
+    "url": "/mondyhallpdx/static/js/main.27adc99e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/mondyhallpdx/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "1715448615d97df2a2ae",
+    "url": "/mondyhallpdx/static/js/2.17154486.chunk.js"
+  },
+  {
+    "revision": "63d5655e7256744469c8",
+    "url": "/mondyhallpdx/static/js/runtime~main.63d5655e.js"
+  },
+  {
+    "revision": "366d14462a35bc6ce0e0b76c17990a99",
+    "url": "/mondyhallpdx/index.html"
+  }
+];
